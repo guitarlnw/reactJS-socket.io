e972f2dc146e208984baa2790a21824e3d45d07c
Admin <admin@MacBook-Pro-khxng-Admin.local>
2018-02-25 17:40:00 +0700

    add socket.io-client

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import socketIOClient from 'socket.io-client'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      input: '',
+      message: [],
+      endpoint: "http://localhost:9000" // เชื่อมต่อไปยัง url ของ realtime server
+    }
+  }
+
+  componentDidMount = () => {
+    this.response()
+  }
+
+  // เมื่อมีการส่งข้อมูลไปยัง server
+  send = (message) => {
+    const { endpoint, input } = this.state
+    const socket = socketIOClient(endpoint)
+    socket.emit('sent-message', input)
+    this.setState({ input: '' })
+  }
+
+  // รอรับข้อมูลเมื่อ server มีการ update
+  response = () => {
+    const { endpoint, message } = this.state
+    const temp = message
+    const socket = socketIOClient(endpoint)
+    socket.on('new-message', (messageNew) => {
+      temp.push(messageNew)
+      this.setState({ message: temp })
+    })
+  }
+
+  changeInput = (e) => {
+    this.setState({ input: e.target.value })
+  }
+
   render() {
+    const { input, message } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div style={style}>
+          <input value={input} onChange={this.changeInput} />
+          <button onClick={() => this.send()}>Send</button>
+        </div>
+        {
+          message.map((data, i) =>
+            <div key={i} style={style} >
+              {i + 1} : {data}
+            </div>
+          )
+        }
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const style = { marginTop: 20, paddingLeft: 50 }
+
+export default App
